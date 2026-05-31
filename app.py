576a7cb3cea84333f5ff749fa2bd4b25c80e597d
EivindJLG <eivindjlgarcia@gmail.com>
2026-05-31 17:39:45 +0200

    oppdatering

--- app.py
+++ app.py
@@ -1,50 +1,68 @@
 from flask import Flask, render_template, request, redirect
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 from db import db, cursor
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return redirect('login')
+    return redirect("/login")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
     if request.method == "POST":
+
         username = request.form["username"]
         password = request.form["password"]
-        
+
         cursor.execute(
-            "SELECT * FROM users WHERE username = %s", 
+            "SELECT * FROM users WHERE username = %s",
             (username,)
         )
 
         user = cursor.fetchone()
 
         if user and check_password_hash(user["password_hash"], password):
+
             cursor.execute(
                 """
                 INSERT INTO logs
                 (event_type, message, severity)
                 VALUES (%s, %s, %s)
                 """,
-            (
-            "LOGIN_SUCCESS",
-            f"User {username} logget inn",
-        "INFO"
+                (
+                    "LOGIN_SUCCESS",
+                    f"Bruker {username} logget inn",
+                    "INFO"
+                )
             )
-        )
+
             db.commit()
 
             return redirect("/dashboard")
-        
+
+        cursor.execute(
+            """
+            INSERT INTO logs
+            (event_type, message, severity)
+            VALUES (%s, %s, %s)
+            """,
+            (
+                "LOGIN_FAIL",
+                f"Feil brukernavn eller passord for {username}",
+                "WARNING"
+            )
+        )
+
+        db.commit()
+
         return "Feil brukernavn eller passord"
-    return render_template("login.html")
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
+        return render_template("login.html")
+    
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
